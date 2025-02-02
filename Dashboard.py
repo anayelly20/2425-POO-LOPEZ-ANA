84ee9521be03fc5cdfc6c608ed75e6d3b1a71a2b
Offer PC <an.lopeza@uea.edu.ec>
2025-02-01 23:17:37 -0500

    Deber semana 8

---
+++ Dashboard.py
@@ -0,0 +1,51 @@
+import os
+
+def mostrar_codigo(ruta_script):
+    # Asegúrate de que la ruta al script es absoluta
+    ruta_script_absoluta = os.path.abspath(ruta_script)
+    try:
+        with open(ruta_script_absoluta, 'r') as archivo:
+            print(f"\n--- Código de {ruta_script} ---\n")
+            print(archivo.read())
+    except FileNotFoundError:
+        print("El archivo no se encontró.")
+    except Exception as e:
+        print(f"Ocurrió un error al leer el archivo: {e}")
+
+
+def mostrar_menu():
+    # Define la ruta base donde se encuentra el dashboard.py
+    ruta_base = os.path.dirname(__file__)
+
+    opciones = {
+        '1': 'SEMANA 02/Semana 2.py',
+        '2': 'SEMANA 03/Semana 3.py',
+        '3': 'SEMANA 04/Semana 4.py',
+        '4': 'SEMANA 05/Semana 5.py',
+        '5': 'SEMANA 06/Semana 6.py',
+        '6': 'SEMANA 07/Semana 7.py',
+
+        # Agrega aquí el resto de las rutas de los scripts
+    }
+
+    while True:
+        print("\n********Menu Principal - Dashboard*************")
+        # Imprime las opciones del menú
+        for key in opciones:
+            print(f"{key} - {opciones[key]}")
+        print("0 - Salir")
+
+        eleccion = input("Elige un script para ver su código o '0' para salir: ")
+        if eleccion == '0':
+            break
+        elif eleccion in opciones:
+            # Asegura que el path sea absoluto
+            ruta_script = os.path.join(ruta_base, opciones[eleccion])
+            mostrar_codigo(ruta_script)
+        else:
+            print("Opción no válida. Por favor, intenta de nuevo.")
+
+
+# Ejecutar el dashboard
+if __name__ == "__main__":
+    mostrar_menu()
